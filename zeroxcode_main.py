54baef22df32fd9e9af51b24d2fc0cc0e804ba7d
zeroqxq <savely-2010-mymrin@yandex.ru>
2025-01-31 21:24:44 +0300

    for 0.0.1_alpha
    setvar
    vars.list

--- zeroxcode_main.py
+++ zeroxcode_main.py
@@ -1,38 +1,32 @@
 #Main vars
 import os
 com = 0
-vars_list = []
+vars = {}
 arg = 0
 args = []
 pr_temp = ""
 total_arg = ""
 input_text = ""
-vars = []
-
-def setvar(name, type , value):
+def setvar(name , type , value):
    try:
       if type == "int":
-            value = int(value)
-            vars.append(name)
-            vars.append(value)
-      elif type == "str":
-         if isinstance(value, str) == True:
-            value = str(value)
-            vars.append(name)
-            vars.append(value)
-      elif type == "bool":
-         if isinstance(value, bool) == True:
-            value = bool(value)
-            vars.append(name)
-            vars.append(value)
+         value = int(value)
+         vars.update({name : value})
+      elif type == "str" or type == "" or type== " ":
+         value = str(value)
+         vars.update({name : value})
+      elif type == "float":
+         value = float(value)
+         vars.update({name : value})
    except:
-      print('Error. Process: setavar')
+      print('Error. Main process: setvar')
 
 def delvar(delit):
-   index = vars.index(delit)
-   vars.remove(delit)
-   vars.pop(index+1)
-   print(vars)
+   try:
+      vars.pop(delit)
+   except:
+      print("Error. Variable not found")
+
 def printer(args):
    global total_arg
    try:
@@ -42,8 +36,8 @@ def printer(args):
          else:
             total_arg += arg + " "
       print(total_arg)
-   except Exception as e:
-      print(f"Error. Description: {e}")
+   except:
+      print("Error. Main process: print ")
 
 os.system("cls")
 print("ZeroxCode v.0.0.1_alpha for x64")
@@ -64,7 +58,10 @@ while True:
          var_type = input("var_type>>")
          var_value = input("var_value>>")
          setvar(var_name, var_type, var_value)
-         print(vars)
+      elif com == "vars_list":
+         for keys in vars.keys():
+            print(keys , end=" ")
+         print("" , sep="\n")
       elif com == "delvar":
          namedel = input("name_var>>")
          delvar(namedel)
