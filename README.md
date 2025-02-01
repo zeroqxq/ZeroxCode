1d8871710889ff3a31f99bd074f003d0d64da2bd
zeroqxq <savely-2010-mymrin@yandex.ru>
2025-02-01 16:19:31 +0300

    0.0.2_alpha

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # ZeroxCode
+Error feedback - [text](https://forms.gle/x1yTLedLwUqUezuZ7)\
+
+Evaluation of application - [text](https://forms.gle/7mwg8rYfstwtAniG6)
+
+Telegram channel - [text](https://t.me/+kLy8wjlzI7xlOWM6)
+
 
 To run the program open - zeroxcode_main.exe.
 
@@ -7,24 +13,32 @@ If “>>” you can enter a command. If the command entered is correct the progr
 
 Functions in cosole:
 
-    setvar:
-            the first argument is the variable name
-            second argument - variable type (str, int , float)
-            third argument - variable value
+        setvar:
+                the first argument is the variable name
+                second argument - variable type (str, int , float)
+                third argument - variable value
 
-    delvar:
-            first argument - name of the variable you want to delete.
+        delvar:
+                first argument - name of the variable you want to delete.
     
-    print:
-            first argument - print argument.
-            You can enter arguments until you enter an empty string
-            If you enter    &last_input    you will output the last input request by the user
-            You can enter a command after the % sign.
-                The following commands are currently available:
-                        %vars_list - list of variables
-                        %last_input - last input from the user
-                        %variable_name - value of the output variable
+        print:
+                first argument - print argument.
+                You can enter arguments until you enter an empty string
+                If you enter    &last_input    you will output the last input request by the user
+                You can enter a command after the % sign.
+                        The following commands are currently available:
+                                %vars_list - list of variables
+                                %last_input - last input from the user
+                                %variable_name - value of the output variable
 
+        input:
+                first argument - Requests input from user
+                second argument - The variable where you want to save the value. If not required, enter an empty string
 
-    input:
-            Requests input from user
+        max_var:
+                first argument -  the name of the first already created variable with type int
+                second argument -  the name of the second already created variable with type int
+        
+        min_var:
+                first argument -  the name of the first already created variable with type int
+                second argument -  the name of the second already created variable with type int
--- zeroxcode_main.py
+++ zeroxcode_main.py
@@ -11,18 +11,29 @@ input_text = ""
 
 def setvar(name , type , value):
    try:
-      if type == "int":
-         value = int(value)
-         vars.update({name : value})
-      elif type == "str" :
-         value = str(value)
-         vars.update({name : value})
-      elif type == "float":
-         value = float(value)
-         vars.update({name : value})
-   except:
-      print('Error. Main process: setvar')
-
+      if name != "" or name.startswith(" "): 
+         if type == "int":
+            value = int(value)
+            vars.update({name : value})
+         elif type == "str" :
+            value = str(value)
+            vars.update({name : value})
+         elif type == "float":
+            value = float(value)
+            vars.update({name : value})
+         else:
+            print("Incorrect variable type")
+      else:
+         print("A variable cannot start with a space or contain spaces, nor can the variable name be empty.")
+   except Exception as e:
+      print(f'Error. Main process: setvar. Description: {e}')
+def inputer(text , svar):
+   global input_text , save_var
+   if text != "" or text != " ":
+      if save_var in vars:
+         vars.update({svar : text})
+      else:
+         vars.update({svar : text})
 def delvar(delit):
    try:
       vars.pop(delit)
@@ -42,13 +53,17 @@ def printer(args):
             elif narg == "vars_list":
                for keys in vars.keys():
                   print(keys , end=" ")
-                  print("" , sep="\n")
+                  print("" , end="\n")
+            elif narg == "vars_items":
+               for key,value in vars.items():
+                  print(key , " : " , value, end=" ; ")
+                  print("" , end="\n")
          else:
             total_arg += str(arg) + " "
       print(total_arg)
       total_arg = ""
-   except:
-      print("Error. Main process: print")
+   except Exception as e:
+      print(f"Error. Main process: print. Description: {e}")
    
 os.system("cls")
 print("ZeroxCode v.0.0.1_alpha for x64")
@@ -77,31 +92,66 @@ while True:
       elif com == "input":
          input_text= ""
          input_text = input("input>> ")
+         save_var = input("Save_var. If not required - enter>>")
+         inputer(input_text, save_var)
       elif com == "max_var":
-         var1name = input("var1_name>>")
-         var2name = input("var2_name>>")
-         if (var1name in vars and var2name in vars):
-            temp1v = vars[var1name]
-            temp2v = vars[var2name]
-            if isinstance(temp1v , int) and isinstance(temp2v , int):
-               maxvar = max(temp2v, temp1v)
-               if maxvar in vars.values():
-                     pass
-               
-               temp1v = ""
-               temp2v = ""
+         try:
+            var1name = input("var1_name>>")
+            var2name = input("var2_name>>")
+            if (var1name in vars and var2name in vars):
+               temp1v = vars[var1name]
+               temp2v = vars[var2name]
+               if isinstance(temp1v , int) and isinstance(temp2v , int):
+                  maxvar = max(temp2v, temp1v)
+                  if maxvar in vars.values():
+                        if maxvar == temp1v:
+                           print("Max variable: ", var1name , "Value: " , maxvar)
+                        elif maxvar == temp2v: 
+                           print("Max variable: ", var2name , "Value: " , maxvar)
+                  temp1v = ""
+                  temp2v = ""
+               else:
+                  print("Error. The variable type is not an int type")
+                  temp1v = ""
+                  temp2v = ""
             else:
-               print("Error. The variable type is not an int type")
-               temp1v = ""
-               temp2v = ""
-         else:
-            print("These variables do not exist")
+               print("These variables do not exist")
+         except Exception as e:
+            print(f"Error. Procces: max_var. Description: {e}")
 
+      elif com == "min_var":
+         try:
+            var1name = input("var1_name>>")
+            var2name = input("var2_name>>")
+            if (var1name in vars and var2name in vars):
+               temp1v = vars[var1name]
+               temp2v = vars[var2name]
+               if isinstance(temp1v , int) and isinstance(temp2v , int):
+                  maxvar = min(temp2v, temp1v)
+                  if maxvar in vars.values():
+                        if maxvar == temp1v:
+                           print("Min variable: ", var1name , "Value: " , maxvar)
+                        elif maxvar == temp2v: 
+                           print("Min variable: ", var2name , "Value: " , maxvar)
+                  temp1v = ""
+                  temp2v = ""
+               else:
+                  print("Error. The variable type is not an int type")
+                  temp1v = ""
+                  temp2v = ""
+            else:
+               print("These variables do not exist")
+         except Exception as e:
+            print(f"Error. Procces: max_var. Description: {e}")
       elif com == "clear":
          os.system("cls")
          print("ZeroxCode v.0.0.1_alpha for x64")
+      elif com == "exit":
+         raise SystemExit(0)
       else:
-         print("Error: incorrect name command")
+         print("Error: Incorrect name command")
+   except SystemExit:
+      raise SystemExit(0)
    except Exception as e:
         print(f"Error. Description - {e}")
 
